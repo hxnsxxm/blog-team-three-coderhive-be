f2e967a874fcdd50e9769a5bb93657f9c26a8b52
hxnsxxm <hansaemj20@gmail.com>
2023-03-17 14:15:26 +0900

    feat: "지역 정보 불러오기 기능 구현"
    지역 정보 목록(code, korean) 불러오기 기능 구현 연습
    
    Resolves: #2

---
+++ src/main/java/net/blogteamthreecoderhivebe/Contoller/Location.java
@@ -0,0 +1,22 @@
+package net.blogteamthreecoderhivebe.Contoller;
+
+import net.blogteamthreecoderhivebe.Dto.LocationDto;
+import net.blogteamthreecoderhivebe.Enum.EnumLocation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/locations")
+public class Location {
+
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public ResponseEntity<List<LocationDto>> getLocationList() {
+        List<LocationDto> locations = EnumLocation.BUSAN.getLocationList();
+        return new ResponseEntity<>(locations, HttpStatus.OK);
+    }
+}
