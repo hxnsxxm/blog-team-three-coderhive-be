d5b4c9fb0178e6165b64aac0a8cbeb378cfe6a76
hxnsxxm <hansaemj20@gmail.com>
2023-03-21 17:22:29 +0900

    feat: 지원 기능 구현 중 리디렉션 여부 확인
    리디렉션을 통해서 GET 요청 가능 여부 확인

--- src/main/java/net/blogteamthreecoderhivebe/Contoller/JobController.java
+++ src/main/java/net/blogteamthreecoderhivebe/Contoller/JobController.java
@@ -2,12 +2,14 @@ package net.blogteamthreecoderhivebe.Contoller;
 
 import net.blogteamthreecoderhivebe.Dto.JobDto;
 import net.blogteamthreecoderhivebe.Enum.EnumJob;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,5 +19,13 @@ public class JobController {
     public ResponseEntity<List<JobDto>> getJobList() {
         List<JobDto> jobList = EnumJob.JOB_DESIGN.getJobList();
         return new ResponseEntity<>(jobList, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/redirect", method= RequestMethod.POST)
+    public ResponseEntity<String> redirect() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/jobs"));
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        //return new ResponseEntity<>("/jobs", HttpStatus.FOUND);
     }
 }
