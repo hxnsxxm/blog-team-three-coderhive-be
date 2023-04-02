88854044076d7a960fa1a40e0ea5593874e5a2b9
hxnsxxm <hansaemj20@gmail.com>
2023-04-03 03:02:33 +0900

    feat: 젠킨스 빌드 테스트

--- src/test/java/net/blogteamthreecoderhivebe/BlogTeamThreeCoderHiveBeApplicationTests.java
+++ src/test/java/net/blogteamthreecoderhivebe/BlogTeamThreeCoderHiveBeApplicationTests.java
@@ -3,10 +3,8 @@ package net.blogteamthreecoderhivebe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BlogTeamThreeCoderHiveBeApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
