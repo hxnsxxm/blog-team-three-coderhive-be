ca5e9a222df3b052d66cb9f4c21621ae02e49dce
hxnsxxm <hansaemj20@gmail.com>
2023-04-03 12:50:25 +0900

    feat: 젠킨스 빌드 테스트

--- src/main/java/net/blogteamthreecoderhivebe/controller/MemberController.java
+++ src/main/java/net/blogteamthreecoderhivebe/controller/MemberController.java
@@ -32,7 +32,7 @@ public class MemberController {
     public MemberInfoWithPostResponse searchMemberInfo(@RequestParam Long memberId, @RequestParam Long searchMemberId) {
         MemberWithPostDto memberWithPostDto = memberService.searchMemberInfoAll(searchMemberId);
         List<Long> postIds = likePostService.searchLikePost(memberId);
-        System.out.println("d");
+        System.out.println("dq");
         return MemberInfoWithPostResponse.from(memberWithPostDto, postIds);
     }
 }
